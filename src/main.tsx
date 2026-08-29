48121b834f9e286ce7ed4e4d3ffd21cf3e407b30
Subhojit Karmakar <essjaykay755@gmail.com>
2026-08-29 10:31:01 +0530

    fix: reset page scroll at turn handoff

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import './coverTransition.css'
 import './loading.css'
+import './pageTurnStability'
 import App from './App.tsx'
 import { getPublicPath, getTermRoutePath } from './utils/ogImage'
 
