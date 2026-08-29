e1ea12b371080f8a774babf90ceb6f383e2020a3
Subhojit Karmakar <essjaykay755@gmail.com>
2026-08-29 10:13:36 +0530

    fix: load cover transition timing styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode, Suspense } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import './coverTransition.css'
 import App from './App.tsx'
 import { getPublicPath, getTermRoutePath } from './utils/ogImage'
 
